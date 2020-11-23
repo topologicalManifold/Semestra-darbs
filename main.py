c419f45d8a0c0874fd55b8cf171cf64e0066887e
Antons Nikolajevs <antons.nikolajevs@gmail.com>
2020-11-23 21:04:25 +0200

    added config file

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import os
 import glob
+import json
 
 from kivymd.app import MDApp
 from kivy.lang.builder import Builder
 from kivy.uix.screenmanager import Screen, ScreenManager
 import kivy.properties as properties
 
-
+# load config file
+with open("config.json", 'r') as config_file:
+    data = json.load(config_file)
 
 class MainScreen(Screen):
     pass
@@ -16,6 +19,7 @@ class MainScreen(Screen):
 class MainApp(MDApp):
     main_text_color = properties.ColorProperty(None)
     def build(self):
+        self.theme_cls.theme_style = data['theme']
         self.theme_cls.primary_palette = "BlueGray"
         self.title = "Lēts Photoshop"
 
@@ -25,14 +29,9 @@ class MainApp(MDApp):
         return self.screen_manager
 
 
-
-    def update_text_color(self):
-        if self.theme_cls.theme_style == "Dark":
-            self.main_text_color = (1, 1, 1, 1)
-        else:
-            self.main_text_color = (0, 0, 0, 1)
-
-
+    def write_data_to_config_file(self):
+        with open("config.json", "w") as config_file:
+            json.dump(data, config_file)
 
 if __name__ == "__main__":
     for file_path in glob.glob(os.path.join("screens", "*.kv")):
